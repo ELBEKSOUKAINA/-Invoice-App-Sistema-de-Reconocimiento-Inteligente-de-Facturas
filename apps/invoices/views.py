bc06511f64841445010c1f48658de507459fe107
Soukaina El Bekbachi <125611154+ELBEKSOUKAINA@users.noreply.github.com>
2025-10-28 20:18:19 +0100

    Convert invoice views to class-based views

--- apps/invoices/views.py
+++ apps/invoices/views.py
@@ -1,12 +1,28 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.urls import reverse_lazy
 from .models import Invoice
+from .forms import InvoiceForm
 
-def invoice_list(request):
-    """Vista básica para listar facturas"""
-    invoices = Invoice.objects.all()
-    return render(request, 'invoices/invoice_list.html', {'invoices': invoices})
+# Lista simple de facturas
+class InvoiceListView(ListView):
+    model = Invoice
+    template_name = 'invoices/invoice_list.html'
 
-def invoice_detail(request, invoice_id):
-    """Vista básica para ver detalle de factura"""
-    invoice = Invoice.objects.get(id=invoice_id)
-    return render(request, 'invoices/invoice_detail.html', {'invoice': invoice})
+# Ver detalles de una factura
+class InvoiceDetailView(DetailView):
+    model = Invoice
+    template_name = 'invoices/invoice_detail.html'
+
+# Crear nueva factura
+class InvoiceCreateView(CreateView):
+    model = Invoice
+    form_class = InvoiceForm
+    template_name = 'invoices/invoice_form.html'
+    success_url = reverse_lazy('invoices:invoice_list')
+
+# Editar factura existente
+class InvoiceUpdateView(UpdateView):
+    model = Invoice
+    form_class = InvoiceForm
+    template_name = 'invoices/invoice_form.html'
+    success_url = reverse_lazy('invoices:invoice_list')

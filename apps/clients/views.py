767c89e4d2c89541735fb5abcf7866f2797139c7
Soukaina El Bekbachi <125611154+ELBEKSOUKAINA@users.noreply.github.com>
2025-10-28 20:15:15 +0100

    Add client views for list, detail, create, and update

--- apps/clients/views.py
+++ apps/clients/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.urls import reverse_lazy
 from .models import Client
+from .forms import ClientForm
 
-# def client_list(request):
-#     clients = Client.objects.all()
-#     return render(request, 'clients/list.html', {'clients': clients})
+# Lista simple de clientes
+class ClientListView(ListView):
+    model = Client
+    template_name = 'clients/client_list.html'
 
-# Añadiremos las vistas más tarde
+# Ver detalles de un cliente
+class ClientDetailView(DetailView):
+    model = Client
+    template_name = 'clients/client_detail.html'
+
+# Crear nuevo cliente
+class ClientCreateView(CreateView):
+    model = Client
+    form_class = ClientForm
+    template_name = 'clients/client_form.html'
+    success_url = reverse_lazy('clients:client_list')
+
+# Editar cliente existente
+class ClientUpdateView(UpdateView):
+    model = Client
+    form_class = ClientForm
+    template_name = 'clients/client_form.html'
+    success_url = reverse_lazy('clients:client_list')
